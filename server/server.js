1c132d9975f44ee954275f1d628940f52bedf94e
Mkuu205 <Ndegwabrian022@gmail.com>
2026-02-08 15:30:18 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 // Import routes
 const authRoutes = require('./routes/auth');
+const syncRoutes = require('./routes/sync');
 const portfolioRoutes = require('./routes/portfolio');
 const galleryRoutes = require('./routes/gallery');
 const bundleRoutes = require('./routes/bundles');
@@ -35,6 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // API Routes
+app.use('/api/sync', syncRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/portfolio', portfolioRoutes);
 app.use('/api/gallery', galleryRoutes);
